fd4c72fda04673b4c228915d68ba6b55ee9341ac
Gem-coder-arch <Jemimaalce821@gmail.com>
2026-08-20 15:10:43 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const dateEl = document.getElementById('today-date');
-if (dateEl) {
-  const today = new Date();
-  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-  dateEl.textContent = today.toLocaleDateString('fr-FR', options);
-}
